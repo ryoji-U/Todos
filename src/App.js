6151d2857705046f194c4b332f82d9f1ce5e7204
ryoji-U <ryoji.australianopen.090203@gmail.com>
2021-09-03 12:12:11 +0900

    タスクの追加と削除機能を実装

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useRef} from 'react';
 
 function App() {
+  const [array1,setArray1] = useState([]);
+  const textRef = useRef(null);
+
+  // タスクを追加
+  const addTodo = (value) => {
+    if(value !== ""){
+      setArray1([...array1,value]);
+      textRef.current.value = "";
+    }
+  }
+
+  // タスクを削除
+  const deleteTodo = (i) => {
+    const newArray1 = [...array1];
+    newArray1.splice(i, 1);
+    setArray1(newArray1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todos">
+      <div>
+
+        <h2>タスクを追加</h2>
+        <input ref={textRef} type="text" />
+        <button onClick={() => addTodo(textRef.current.value)}>追加</button>
+
+        <div className="spacer_30"></div>
+
+        <h2>タスク</h2>
+        {array1.map((task,i) => (
+          <div>
+            <input type="text" value={task}/>
+            <button onClick={() => deleteTodo(i)}>削除</button>
+          </div>
+        ))}
+
+      </div>
     </div>
   );
 }
